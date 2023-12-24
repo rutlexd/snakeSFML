06b47ac24103243a7f2bf38c1a3c5dca4f287be1
rutlexd <maaks.minecrafter@gmail.com>
2023-12-24 13:34:19 +0200

    create class Snake, load textures

--- objects/background.cpp
+++
@@ -1,9 +0,0 @@
-#include "/home/rutle/progs/snake/assets.cpp"
-
-AssetsHolder assets;
-
-void init(){
-    Texture backgroundImage;
-
-    backgroundImage.loadFromFile("asssets/sprites/background.png");
-}
